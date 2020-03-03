5f9ec08e6b888735a63481b95b14b0c50aa6a9e8
Duan Rafael Ribeiro <duan.ribeiro@hotmail.com>
2020-03-03 15:23:46 -0300

    Bug, correct to port 8000

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=8000)
 
 else:
     gunicorn_logger = logging.getLogger('gunicorn.error')
